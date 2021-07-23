fb941244d5aaa636a5b8ff601522ad9aaee4de60
shubhasankar <ssbhaumikdmr@gmail.com>
2021-07-24 00:03:47 +0530

    Added: Create Address Book Table

--- AddressBook_services.sql
+++ AddressBook_services.sql
@@ -2,3 +2,17 @@
 create database AddressBook_services;
 show databases;
 use AddressBook_services;
+
+#uc2
+create table Address_book(
+	id int unsigned not null auto_increment,
+	fname varchar(15) not null,
+    lname varchar(10) not null,
+    city varchar(50) not null,
+    state varchar(50) not null,
+    zip int not null,
+    phone long not null,
+    email varchar(100) not null,
+     primary key (id)
+);
+select * from address_book;
